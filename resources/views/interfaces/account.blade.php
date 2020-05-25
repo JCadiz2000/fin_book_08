f6452b37fc805c9fc1c3f254d467799240879519
Jayson Cadiz <cadizjayson25@yahoo.com>
2020-05-25 17:04:15 +0800

    Updated

--- resources/views/interfaces/account.blade.php
+++ resources/views/interfaces/account.blade.php
@@ -1,84 +1,68 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Category Management</title>
-    <!-- Styles -->
-    <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="vendor/datatables/dataTables.bootstrap4.min.css">
-    <!-- Styles -->
-</head>
-<body id="page-top">
+@include('base.header')
     <!-- Start -->
         <!-- Page Wrapper -->
         <div id="wrapper">
 
             <!-- Sidebar -->
-            <ul class="navbar-nav bg-gradient-success sidebar sidebar-dark accordion" id="accordionSidebar">
+                <ul class="navbar-nav bg-gradient-success sidebar sidebar-dark accordion" id="accordionSidebar">
 
-            <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/account">
-                <div class="sidebar-brand-icon rotate-n-15">
-                <i class="fas fa-dollar-sign"></i>
+                <!-- Sidebar - Brand -->
+                <a class="sidebar-brand d-flex align-items-center justify-content-center" href="/account">
+                    <div class="sidebar-brand-icon rotate-n-15">
+                    <i class="fas fa-dollar-sign"></i>
+                    </div>
+                    <div class="sidebar-brand-text mx-3">FinBook</div>
+                </a>
+
+                <!-- Divider -->
+                <hr class="sidebar-divider my-0">
+
+                <!-- Nav Item - Dashboard -->
+                <li class="nav-item active ">
+                    <a class="nav-link" href="/account">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Dashboard</span></a>
+                </li>
+
+                <!-- Divider -->
+                <hr class="sidebar-divider">
+
+                <!-- Heading -->
+                <div class="sidebar-heading">
+                    Menu
                 </div>
-                <div class="sidebar-brand-text mx-3">FinBook</div>
-            </a>
 
-            <!-- Divider -->
-            <hr class="sidebar-divider my-0">
+                <!-- Category Menu Item -->
+                <li class="nav-item">
+                    <a class="nav-link" href="/account/category">
+                    <i class="fas fa-fw fa-list"></i>
+                    <span>Category Management</span></a>
+                </li>
 
-            <!-- Nav Item - Dashboard -->
-            <li class="nav-item active ">
-                <a class="nav-link" href="/account">
-                <i class="fas fa-fw fa-tachometer-alt"></i>
-                <span>Dashboard</span></a>
-            </li>
+                <!-- Ledger Menu Item -->
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                    <i class="fas fa-fw fa-balance-scale"></i>
+                    <span>Ledger</span></a>
+                </li>
 
-            <!-- Divider -->
-            <hr class="sidebar-divider">
+                <!-- Monthly Menu Item -->
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                    <i class="far fa-fw fa-calendar"></i>
+                    <span>Monthly Report</span></a>
+                </li>
 
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                Menu
-            </div>
 
-            <!-- Category Menu Item -->
-            <li class="nav-item">
-                <a class="nav-link" href="#">
-                <i class="fas fa-fw fa-list"></i>
-                <span>Category Management</span></a>
-            </li>
+                <!-- Divider -->
+                <hr class="sidebar-divider d-none d-md-block">
 
-            <!-- Ledger Menu Item -->
-            <li class="nav-item">
-                <a class="nav-link" href="#">
-                <i class="fas fa-fw fa-balance-scale"></i>
-                <span>Ledger</span></a>
-            </li>
+                <!-- Sidebar Toggler (Sidebar) -->
+                <div class="text-center d-none d-md-inline">
+                    <button class="rounded-circle border-0" id="sidebarToggle"></button>
+                </div>
 
-            <!-- Monthly Menu Item -->
-            <li class="nav-item">
-                <a class="nav-link" href="#">
-                <i class="far fa-fw fa-calendar"></i>
-                <span>Monthly Report</span></a>
-            </li>
-           
-
-            <!-- Divider -->
-            <hr class="sidebar-divider d-none d-md-block">
-
-            <!-- Sidebar Toggler (Sidebar) -->
-            <div class="text-center d-none d-md-inline">
-                <button class="rounded-circle border-0" id="sidebarToggle"></button>
-            </div>
-
-            </ul>
+                </ul>
             <!-- End of Sidebar -->
 
             <!-- Content Wrapper -->
@@ -338,25 +322,49 @@
                                             </thead>
                                             
                                             <tbody>
-                                                <tr>
+                                                <tr class="table-success">
                                                     <td>March 23, 2020</td>
                                                     <td>My Salary from work</td>
                                                     <td>Work Salary</td>
                                                     <td><span>₱</span>5,000</td>
                                                 </tr>
-                                                <tr>
+                                                <tr class="table-danger">
                                                     <td>March 24, 2020</td>
                                                     <td>Breakfast</td>
                                                     <td>Food</td>
                                                     <td><span>(<span>₱</span>500)</span></td>
                                                 </tr>
-                                                <tr>
+                                                <tr class="table-danger">
                                                     <td>March 24, 2020</td>
                                                     <td>Lunch</td>
                                                     <td>Food</td>
                                                     <td><span>(<span>₱</span>250)</span></td>
                                                 </tr>
-                                                <tr>
+                                                <tr class="table-danger">
+                                                    <td>March 24, 2020</td>
+                                                    <td>Dinner</td>
+                                                    <td>Food</td>
+                                                    <td><span>(<span>₱</span>1,000)</span></td>
+                                                </tr>
+                                                <tr class="table-success">
+                                                    <td>March 23, 2020</td>
+                                                    <td>My Salary from work</td>
+                                                    <td>Work Salary</td>
+                                                    <td><span>₱</span>5,000</td>
+                                                </tr>
+                                                <tr class="table-danger">
+                                                    <td>March 24, 2020</td>
+                                                    <td>Breakfast</td>
+                                                    <td>Food</td>
+                                                    <td><span>(<span>₱</span>500)</span></td>
+                                                </tr>
+                                                <tr class="table-danger">
+                                                    <td>March 24, 2020</td>
+                                                    <td>Lunch</td>
+                                                    <td>Food</td>
+                                                    <td><span>(<span>₱</span>250)</span></td>
+                                                </tr>
+                                                <tr class="table-danger">
                                                     <td>March 24, 2020</td>
                                                     <td>Dinner</td>
                                                     <td>Food</td>
@@ -418,33 +426,23 @@
         </div>
         
     <!-- End -->
-<!-- Bootstrap core JavaScript-->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  <!-- Core plugin JavaScript-->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-  <!-- Custom scripts for all pages-->
-  <script src="js/sb-admin-2.min.js"></script>
-
-  <!-- Page level plugins -->
-  <script src="vendor/chart.js/Chart.min.js"></script>
-  <script src="vendor/datatables/jquery.dataTables.min.js"></script>
-  <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
-
-  <!-- Page level custom scripts -->
-  <script src="js/demo/chart-area-demo.js"></script>
-  <script src="js/demo/chart-pie-demo.js"></script>
-
-  <script src="js/demo/datatables-demo.js"></script>
-  <script>
-      $(document).ready(function() {
-        $('#categoryList').DataTable();    
-      });
-      $(document).ready(function() {
-        $('#ledgerList').DataTable();    
-      });
-  </script>
-</body>
+    
+        @include('base.footer')
+        <script>
+        $(document).ready(function() {
+            $('#categoryList').DataTable({
+                ordering: false,
+                lengthMenu:[[5,10,25,50,-1],[5,10,25,50,'All']],
+                searching: false
+            });    
+        });
+        $(document).ready(function() {
+            $('#ledgerList').DataTable({
+                ordering: false,
+                lengthMenu:[[5,10,25,50,-1],[5,10,25,50,'All']],
+                searching: false
+            });    
+        });
+    </script>
+    </body>
 </html>
