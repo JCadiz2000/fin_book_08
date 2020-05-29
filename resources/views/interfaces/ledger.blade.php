b25a9bcaefa855de8d1aef57f8a897316a6aaa2b
Jayson Cadiz <cadizjayson25@yahoo.com>
2020-05-29 21:08:39 +0800

    Updated

--- resources/views/interfaces/ledger.blade.php
+++ resources/views/interfaces/ledger.blade.php
@@ -5,7 +5,7 @@
 	<!-- Sidebar -->
 	<ul class="navbar-nav bg-gradient-success sidebar sidebar-dark accordion" id="accordionSidebar">
 		<!-- Sidebar - Brand -->
-		<a class="sidebar-brand d-flex align-items-center justify-content-center" href="/account">
+		<a class="sidebar-brand d-flex align-items-center justify-content-center" href="/">
 			<div class="sidebar-brand-icon rotate-n-15">
 				<i class="fas fa-dollar-sign"></i>
 			</div>
