653539af4059ff9572f5b20cac63d61609d66925
Jayson Cadiz <cadizjayson25@yahoo.com>
2020-05-29 20:56:56 +0800

    Updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -120,8 +120,8 @@
                     <div class="container-fluid p-0">
                         <div class="row no-gutters">
                             <div class="col-lg-4 col-sm-6">
-                                <a class="portfolio-box" href="assets/img/portfolio/fullsize/1.jpg"
-                                    ><img class="img-fluid" src="assets/img/portfolio/thumbnails/1.jpg" alt="" />
+                                <a class="portfolio-box" href="#"
+                                    ><img class="img-fluid" src="assets/images/Report.png" alt="" />
                                     <div class="portfolio-box-caption">
                                         <div class="project-category text-white-50">Category</div>
                                         <div class="project-name">Project Name</div>
@@ -129,8 +129,8 @@
                                 >
                             </div>
                             <div class="col-lg-4 col-sm-6">
-                                <a class="portfolio-box" href="assets/img/portfolio/fullsize/2.jpg"
-                                    ><img class="img-fluid" src="assets/img/portfolio/thumbnails/2.jpg" alt="" />
+                                <a class="portfolio-box" href="#"
+                                    ><img class="img-fluid" src="assets/images/Category.png" alt="" />
                                     <div class="portfolio-box-caption">
                                         <div class="project-category text-white-50">Category</div>
                                         <div class="project-name">Project Name</div>
@@ -138,35 +138,8 @@
                                 >
                             </div>
                             <div class="col-lg-4 col-sm-6">
-                                <a class="portfolio-box" href="assets/img/portfolio/fullsize/3.jpg"
-                                    ><img class="img-fluid" src="assets/img/portfolio/thumbnails/3.jpg" alt="" />
-                                    <div class="portfolio-box-caption">
-                                        <div class="project-category text-white-50">Category</div>
-                                        <div class="project-name">Project Name</div>
-                                    </div></a
-                                >
-                            </div>
-                            <div class="col-lg-4 col-sm-6">
-                                <a class="portfolio-box" href="assets/img/portfolio/fullsize/4.jpg"
-                                    ><img class="img-fluid" src="assets/img/portfolio/thumbnails/4.jpg" alt="" />
-                                    <div class="portfolio-box-caption">
-                                        <div class="project-category text-white-50">Category</div>
-                                        <div class="project-name">Project Name</div>
-                                    </div></a
-                                >
-                            </div>
-                            <div class="col-lg-4 col-sm-6">
-                                <a class="portfolio-box" href="assets/img/portfolio/fullsize/5.jpg"
-                                    ><img class="img-fluid" src="assets/img/portfolio/thumbnails/5.jpg" alt="" />
-                                    <div class="portfolio-box-caption">
-                                        <div class="project-category text-white-50">Category</div>
-                                        <div class="project-name">Project Name</div>
-                                    </div></a
-                                >
-                            </div>
-                            <div class="col-lg-4 col-sm-6">
-                                <a class="portfolio-box" href="assets/img/portfolio/fullsize/6.jpg"
-                                    ><img class="img-fluid" src="assets/img/portfolio/thumbnails/6.jpg" alt="" />
+                                <a class="portfolio-box" href="#"
+                                    ><img class="img-fluid" src="assets/images/Ledger.jpg" alt="" />
                                     <div class="portfolio-box-caption p-3">
                                         <div class="project-category text-white-50">Category</div>
                                         <div class="project-name">Project Name</div>
@@ -182,7 +155,7 @@
                         <div class="row justify-content-center">
                             <div class="col-lg-8 text-center">
                                 <h2 class="mt-0">Let's Get In Touch!</h2>
-                                <hr class="divider my-4" />
+                                <hr class="divider green my-4" />
                                 <p class="text-muted mb-5">Ready to start your next project with us? Give us a call or send us an email and we will get back to you as soon as possible!</p>
                             </div>
                         </div>
